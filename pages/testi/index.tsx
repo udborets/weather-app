c3ece814850270e53589320c7e8d8606bde63588
Yury Borets <udborets@gmail.com>
2023-03-22 19:36:07 +0300

    fix test query

--- pages/testi/index.tsx
+++ pages/testi/index.tsx
@@ -1,16 +1,15 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 
 const Index = () => {
-  const ip = useQuery({
-    queryFn: async () => {
-      const ip = await axios.get("http://ip-api.com/json/?fields=status,message,country,regionName,zip,lat,lon,timezone,isp,org,as,query")
-      return ip.data?.query
-    }
+  const [ip, setIp] = useState<any>();
+  useEffect(() => {
+    axios.get("http://ip-api.com/json/?fields=status,message,country,regionName,zip,lat,lon,timezone,isp,org,as,query")
+      .then((data) => setIp(data.data?.query))
   })
   return (
-    <div>{ip.data}</div>
+    <div>{ip}</div>
   )
 }
 
