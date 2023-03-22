e57f8a3e95855bd913abc6f03308012d50b303b7
Yury Borets <udborets@gmail.com>
2023-03-22 21:11:45 +0300

    move viewport meta tag from NextJS document file

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <Head>
         <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
       </main>
