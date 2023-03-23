c78061f28b0ea9becb6a3bfa23b2f80ccfd37a21
Yury Borets <udborets@gmail.com>
2023-03-23 17:41:07 +0300

    fix ip getting function

--- pages/testi/index.tsx
+++ pages/testi/index.tsx
@@ -1,18 +1,21 @@
-import { getIp } from '@/services/ip'
-import { getWeatherLink } from '@/services/weather'
-import axios from 'axios'
-import React from 'react'
-import { useQuery } from 'react-query'
+import { getIp } from '@/services/ip';
+import { getWeatherLink } from '@/services/weather';
+import axios from 'axios';
+import React from 'react';
+import { useQuery } from 'react-query';
 
 
 const Index = () => {
+  const { data: userIp } = useQuery({
+    queryFn: async () => await getIp()
+  })
   const { data: name } = useQuery({
     queryFn: async () => {
-      const userIp = await getIp();
       if (!userIp) return;
-      const req = (await axios.get(getWeatherLink(userIp))).data
-      return req?.location.tz_id
-    }
+      const req = (await axios.get(getWeatherLink(userIp))).data;
+      return req?.location.tz_id;
+    },
+    refetchInterval: userIp ? 3600000 : 2000,
   })
   return (
     <div>
