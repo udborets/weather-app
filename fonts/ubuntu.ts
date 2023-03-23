17ef0e6c13eb4b12940ef08a9b7cbc9d93089c07
Yury Borets <udborets@gmail.com>
2023-03-23 16:56:28 +0300

    add ubuntu preset fonts

---
+++ fonts/ubuntu.ts
@@ -0,0 +1,11 @@
+import { Ubuntu } from "next/font/google";
+
+export const ubuntuBold = Ubuntu({
+  subsets: ["latin"],
+  weight: "700",
+});
+
+export const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  weight: "400",
+});
