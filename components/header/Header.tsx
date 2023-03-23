f8e6e11149c15d0c134154f48e47f2cc96d9be36
Yury Borets <udborets@gmail.com>
2023-03-23 16:45:58 +0300

    add basic header component

---
+++ components/header/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Ubuntu } from 'next/font/google'
+import Link from 'next/link'
+
+const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  weight: "700",
+})
+
+const Header = () => {
+  return (
+    <header className="header h-[100px] p-4 w-full min-w-full flex justify-center">
+      <div className="header__container flex w-4/5 h-full min-h-ful">
+        <Link href="/" className="w-fit h-fit self-center">
+          <h2 className="header__title w-fit h-fit">
+            <span className={`title__blue text-[1.7rem] pl-3 text-[#089cfc] ${ubuntu.className}`}>
+              My
+            </span>
+            <span className={`title-black text-[1.7rem] text-black pr-3 border-r-[1px] ${ubuntu.className}`}>
+              Weather
+            </span>
+          </h2>
+        </Link>
+      </div>
+    </header >
+  )
+}
+
+export default Header
