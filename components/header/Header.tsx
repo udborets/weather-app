d7e055675fcc751c2f4da17bfbdbcb25a81ec2b0
Yury Borets <udborets@gmail.com>
2023-03-23 19:16:16 +0300

    fix import path

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
 import Navbar from '@/components/navbar/Navbar';
-import NavbarLink from '@/components/NavbarLink/NavbarLink';
+import NavbarLink from '@/components/navbarlink/NavbarLink';
 import { ubuntuBold } from "@/fonts/ubuntu";
 
 const Header = () => {
   return (
-    <header className="header h-[100px] p-4 w-full min-w-full flex justify-center">
+    <header className="header h-[80px] p-4 w-full min-w-full flex justify-center">
       <div className="header__container flex w-4/5 h-full min-h-ful">
         <Link href="/" className="header__title w-fit h-fit self-center ">
           <h2 className="title__text w-fit h-fit">
@@ -18,7 +18,7 @@ const Header = () => {
             </span>
           </h2>
         </Link>
-        <div className="header__split w-[1px] h-3/5 bg-gray-500 self-center mx-6" />
+        <div className="header__split w-[1px] h-1/2 bg-slate-300 self-center mx-6" />
         <Navbar>
           <NavbarLink href="/">
             Home
