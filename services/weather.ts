bc8b8d5e699169a8e77f07aafe6a973d9359f641
Yury Borets <udborets@gmail.com>
2023-03-23 21:11:27 +0300

    fix http query protocol

--- services/weather.ts
+++ services/weather.ts
@@ -1,3 +1,3 @@
 export function getWeatherLink(param: string) {
-  return `http://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY} &q=${param}&aqi=no`;
+  return `https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY} &q=${param}`;
 }
