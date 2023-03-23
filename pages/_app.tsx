cffd2b6cfaf708abc3f0aa5624c14c92baf54d56
Yury Borets <udborets@gmail.com>
2023-03-23 20:22:23 +0300

    wrap root page component with page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,19 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Header from '@/components/header/Header';
+import PageLayout from '@/layouts/PageLayout';
 import '@/styles/global.scss';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <Header />
-        <Component {...pageProps} />
+        <PageLayout>
+          <Component {...pageProps} />
+        </PageLayout>
       </QueryClientProvider>
     </>
   )
