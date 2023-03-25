1fa777421fb53944cba04f0e1b1f69390086a599
Yury Borets <udborets@gmail.com>
2023-03-25 16:02:15 +0300

    change useDebounce hook argument to string only

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
-export function useDebounce(value: any, delay: number) {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+export function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState<string>(value);
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedValue(value);
