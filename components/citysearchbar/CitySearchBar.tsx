c63897dfa8a5affb75cba913111fbb18e84e40d3
Yury Borets <udborets@gmail.com>
2023-03-25 13:34:49 +0300

    change city search bar component width

--- components/citysearchbar/CitySearchBar.tsx
+++ components/citysearchbar/CitySearchBar.tsx
@@ -6,7 +6,7 @@ interface ICitySearchBarProps {
 
 const CitySearchBar = ({ search, setSearch }: ICitySearchBarProps) => {
   return (
-    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search..." className='border-2 text-[1rem] px-2 py-1' />
+    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search..." className='w-[300px] border-2 text-[1rem] px-2 py-1' />
   )
 }
 
