494f93b23ac7d7bf2b05890bddc577cb743e739f
Yury Borets <udborets@gmail.com>
2023-03-27 18:18:27 +0300

    add no result prop to city search bar item

--- components/CitySearchBarItem/CitySearchBarItem.tsx
+++ components/CitySearchBarItem/CitySearchBarItem.tsx
@@ -2,25 +2,35 @@ import { City } from "@/models/city"
 import { useChosenCity } from "@/store/useChosenCity";
 
 interface ICitySearchBarItemProps {
-  city: City;
+  city?: City;
+  noResultText?: string;
   hideSearchResultsFn: () => void;
 }
 
-const CitySearchBarItem = ({ city, hideSearchResultsFn }: ICitySearchBarItemProps) => {
-  const setChosenCity = useChosenCity((store: any) => store.setChosenCity)
+const CitySearchBarItem = ({ city, hideSearchResultsFn, noResultText }: ICitySearchBarItemProps) => {
+  const setChosenCity = useChosenCity((store: any) => store.setChosenCity);
   return (
     <div className="searchBarResult px-3 py-2 border-b-2 
      transition-colors duration-200 last-of-type:border-b-0 text-black bg-white hover:text-white hover:bg-sky w-[300px]"
-      onClick={() => { setChosenCity(city); hideSearchResultsFn() }}
+      onClick={() => {
+        if (city) {
+          setChosenCity(city);
+        }
+        hideSearchResultsFn();
+      }}
     >
       <div className="searchBarResult__container gap-3 flex justify-start relative">
         <span>
-          {city.name.length <= 30
+          {noResultText
+            ? <span>{noResultText}</span>
+            : ''}
+          {city ? city.name.length <= 30
             ? city.name
-            : <span>{city.name.slice(0, 30)}...</span>}
+            : <span>{city.name.slice(0, 30)}...</span>
+            : ''}
         </span>
         <span className='absolute top-[0px] right-0'>
-          {city.country}
+          {city ? city.country : ''}
         </span>
       </div>
     </div>
