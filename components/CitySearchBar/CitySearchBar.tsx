c6c82bae8f6f01f7c656d139000d81b0e4762e44
Yury Borets <udborets@gmail.com>
2023-03-27 20:47:29 +0300

    change border radius

--- components/CitySearchBar/CitySearchBar.tsx
+++ components/CitySearchBar/CitySearchBar.tsx
@@ -62,7 +62,7 @@ const CitySearchBar = () => {
       </div>
       {isShowingSearchResults
         ?
-        <div className="searchBar__results absolute flex flex-col gap-2 border-2 border-gray-500 my-1 rounded-[5px] p-2 w-[300px]">
+        <div className="searchBar__results absolute flex flex-col gap-2 border-2 border-gray-500 my-1 rounded-[10px] p-2 w-[300px]">
           {(cities.data && cities.data.length !== 0)
             ? cities.data.slice(0, 6).map((city) => (
               <CitySearchBarItem city={city} key={city.id} hideSearchResultsFn={() => setIsShowingSearchResults(false)} />
