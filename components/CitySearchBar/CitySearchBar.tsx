fbbd7afc31509cccda27479ea8709812918df094
Yury Borets <udborets@gmail.com>
2023-03-27 07:57:24 +0300

    add loading spinner to city search bar

--- components/CitySearchBar/CitySearchBar.tsx
+++ components/CitySearchBar/CitySearchBar.tsx
@@ -8,6 +8,7 @@ import CitySearchBarItem from '@/components/CitySearchBarItem/CitySearchBarItem'
 import { useDebounce } from '@/hooks/useDebounce';
 import { City } from '@/models/city';
 import { Store, useChosenCity } from '@/store/useChosenCity';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 const CitySearchBar = () => {
   const [search, setSearch] = useState<string>('');
@@ -17,13 +18,18 @@ const CitySearchBar = () => {
   const cities = useQuery({
     queryFn: async () => {
       if (!debouncedSearch) return [];
-      const fetchedCities = await axios.get<City[]>(`/api/cities/${debouncedSearch}`);
-      if (fetchedCities.status !== 200) {
-        console.warn(fetchedCities.data);
-        return [];
+      try {
+        const fetchedCities = await axios.get<City[]>(`/api/cities/${debouncedSearch}`);
+        if (fetchedCities.status !== 200) {
+          console.error(fetchedCities);
+          return [];
+        }
+        return fetchedCities.data;
+        // return []
       }
-      return fetchedCities.data;
-      // return []
+      catch (e: unknown) {
+        console.error(e);
+      }
     },
     refetchOnMount: false,
     refetchOnWindowFocus: false,
@@ -34,25 +40,30 @@ const CitySearchBar = () => {
   }, [debouncedSearch])
   return (
     <div className="searchBar">
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder={cities.isLoading ? "Cities loading..." : "Search..."}
-        className='searchBar__input w-[300px] border-2 text-[1rem] px-2 py-1'
-        disabled={cities.isLoading}
-      />
+      <div className='searchBar__bar flex justify-between w-[300px] border-2 relative'>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder={cities.isLoading ? "Cities loading..." : "Search..."}
+          className='searchBar__input w-full h-full text-[1rem] px-2 py-1'
+          disabled={cities.isLoading || cities.isRefetching}
+        />
+        {(cities.isLoading || cities.isRefetching)
+          ? <LoadingSpinner />
+          : <></>}
+      </div>
       <div className="searchBar__results absolute">
         {cities.data?.slice(0, 6).map((city) => (
           <CitySearchBarItem city={city} key={city.id} />
         ))}
-        <button onClick={() => { if (cities.data?.length) chooseCity(cities.data[2]) }}>
-          haha
-        </button>
-        {
-          chosenCity?.coord?.lon
-        }
       </div>
+      <button onClick={() => { if (cities.data?.length) chooseCity(cities.data[2]) }}>
+        haha
+      </button>
+      {
+        chosenCity?.coord?.lon
+      }
     </div>
   )
 }
