912e8ac5c94c011417c3d70bfb715ea9fa983c89
Yury Borets <udborets@gmail.com>
2023-03-27 16:26:39 +0300

    fix search bar item click fn

--- components/CitySearchBarItem/CitySearchBarItem.tsx
+++ components/CitySearchBarItem/CitySearchBarItem.tsx
@@ -3,26 +3,22 @@ import { useChosenCity } from "@/store/useChosenCity";
 
 interface ICitySearchBarItemProps {
   city: City;
+  hideSearchResultsFn: () => void;
 }
 
-const CitySearchBarItem = ({ city }: ICitySearchBarItemProps) => {
+const CitySearchBarItem = ({ city, hideSearchResultsFn }: ICitySearchBarItemProps) => {
   const setChosenCity = useChosenCity((store: any) => store.setChosenCity)
   return (
-    <div className="searchBarResult px-3 py-2 border-b-2 last-of-type:border-b-0 text-black bg-white hover:text-white hover:bg-sky w-[300px]"
-      onClick={() => setChosenCity(city)}
+    <div className="searchBarResult px-3 py-2 border-b-2 
+     transition-colors duration-200 last-of-type:border-b-0 text-black bg-white hover:text-white hover:bg-sky w-[300px]"
+      onClick={() => { setChosenCity(city); hideSearchResultsFn() }}
     >
       <div className="searchBarResult__container gap-3 flex justify-start relative">
         <span>
-          {city.name}
+          {city.name.length <= 30
+            ? city.name
+            : <span>{city.name.slice(0, 30)}...</span>}
         </span>
-        <div className='flex flex-col absolute top-[-8px] right-[40px]'>
-          <span className="text-[0.8rem]">
-            lat: {city.coord.lat.toString().slice(0, 6)}
-          </span>
-          <span className="text-[0.8rem]">
-            lon: {city.coord.lon.toString().slice(0, 6)}
-          </span>
-        </div>
         <span className='absolute top-[0px] right-0'>
           {city.country}
         </span>
