aa3a8525f9522827a2d7a078c41187fe8a4e5da9
Yury Borets <udborets@gmail.com>
2023-03-27 16:26:59 +0300

    fix search results displaying

--- components/CitySearchBar/CitySearchBar.tsx
+++ components/CitySearchBar/CitySearchBar.tsx
@@ -7,24 +7,34 @@ import { useQuery } from 'react-query';
 import CitySearchBarItem from '@/components/CitySearchBarItem/CitySearchBarItem';
 import { useDebounce } from '@/hooks/useDebounce';
 import { City } from '@/models/city';
-import { Store, useChosenCity } from '@/store/useChosenCity';
+import { useChosenCity } from '@/store/useChosenCity';
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 const CitySearchBar = () => {
   const [search, setSearch] = useState<string>('');
-  const chosenCity = useChosenCity((state: any) => state.chosenCity);
-  const chooseCity = useChosenCity((state: any) => state.setChosenCity);
+  const chosenCity: City = useChosenCity((state: any) => state.chosenCity);
   const debouncedSearch = useDebounce(search, 500);
+  const [isShowingSearchResults, setIsShowingSearchResults] = useState<boolean>(false);
   const cities = useQuery({
     queryFn: async () => {
-      if (!debouncedSearch) return [];
+      if (!debouncedSearch) {
+        setIsShowingSearchResults(false);
+        return [];
+      }
       try {
         const fetchedCities = await axios.get<City[]>(`/api/cities/${debouncedSearch}`);
         if (fetchedCities.status !== 200) {
           console.error(fetchedCities);
           return [];
         }
-        return fetchedCities.data;
+        if (fetchedCities.data.length)
+          setIsShowingSearchResults(true);
+        const cityNames: string[] = [];
+        return fetchedCities.data.filter((city) => {
+          if (cityNames.includes(city.name)) return false;
+          cityNames.push(city.name);
+          return true;
+        })
       }
       catch (e: unknown) {
         console.error(e);
@@ -46,17 +56,19 @@ const CitySearchBar = () => {
           onChange={(e) => setSearch(e.target.value)}
           placeholder={cities.isLoading ? "Cities loading..." : "Search..."}
           className='searchBar__input w-full h-full text-[1rem] px-2 py-1'
-          disabled={cities.isLoading || cities.isRefetching}
         />
         {(cities.isLoading || cities.isRefetching)
           ? <LoadingSpinner />
           : <></>}
       </div>
       <div className="searchBar__results absolute">
-        {Array.isArray(cities.data)
-          ? cities.data.slice(0, 6).map((city) => (
-            <CitySearchBarItem city={city} key={city.id} />
-          ))
+        {isShowingSearchResults
+          ?
+          cities?.data?.length
+            ? cities.data.slice(0, 6).map((city) => (
+              <CitySearchBarItem city={city} key={city.id} hideSearchResultsFn={() => setIsShowingSearchResults(false)} />
+            ))
+            : <div>No results</div>
           : <></>}
       </div>
     </div>
