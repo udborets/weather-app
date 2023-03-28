09b7cfb0ff20c92aad41150f057359b7daa72474
Yury Borets <udborets@gmail.com>
2023-03-28 20:16:30 +0300

    add weather api request type

---
+++ models/weather.ts
@@ -0,0 +1,55 @@
+export type WeatherRequest = {
+  city: {
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    id: number;
+    name: string;
+    population: number;
+    sunrise: number;
+    sunset: number;
+    timezone: number;
+  };
+  cnt: number;
+  cod: string;
+  message: number;
+  list: {
+    clouds: {
+      all: number;
+    };
+    dt: number;
+    dt_txt: string;
+    main: {
+      feels_like: number;
+      grnd_level: number;
+      humidity: number;
+      pressure: number;
+      sea_level: number;
+      temp: number;
+      temp_kf: number;
+      temp_max: number;
+      temp_min: number;
+    };
+    pop: number;
+    rain: {
+      "3h": number;
+    };
+    sys: {
+      pod: string;
+    };
+    visibility: number;
+    weather: {
+      description: string;
+      icon: string;
+      id: number;
+      main: string;
+    }[];
+    wind: {
+      deg: number;
+      gust: number;
+      speed: number;
+    };
+  }[];
+};
