5a461c7ea00cf117111819134b82c7cbc3d86b1c
Yury Borets <udborets@gmail.com>
2023-03-28 20:44:50 +0300

    add dev chosen city name showing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { City } from "@/models/city";
+import { useChosenCity } from "@/store/useChosenCity";
+
 export default function Home() {
+  const chosenCity: City = useChosenCity((store: any) => store?.chosenCity);
   return (
     <div className="w-full h-full flex flex-col">
-
+      {chosenCity.name}
     </div>
   )
 }
