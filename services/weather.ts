db8d36e2c5cc1d3b7b15121088928fdadb52fa9e
Yury Borets <udborets@gmail.com>
2023-03-29 17:59:10 +0300

    add units param to weather query

--- services/weather.ts
+++ services/weather.ts
@@ -4,5 +4,5 @@ export function getWeatherLink({ lat, lon }: { lat: number; lon: number }) {
     console.error("Can't get weather api key");
     return null;
   }
-  return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
+  return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`;
 }
