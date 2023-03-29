af3f4823e3620cf3fde547b70ec51b99fd55f6c6
Yury Borets <udborets@gmail.com>
2023-03-29 17:58:48 +0300

    add weather info bar component

---
+++ components/WeatherInfoBar/WeatherInfoBar.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+import { City } from "@/models/city";
+import { WeatherRequest } from "@/models/weather";
+import { getWeatherLink } from "@/services/weather";
+import { useChosenCity } from "@/store/useChosenCity";
+
+const WeatherInfoBar = () => {
+  const chosenCity: City = useChosenCity((store: any) => store?.chosenCity);
+  const weather = useQuery({
+    queryFn: async () => {
+      const weatherLink = getWeatherLink(chosenCity.coord);
+      if (!weatherLink) {
+        console.error()
+        return null
+      };
+      const fetchedWeather = (await axios.get<WeatherRequest>(weatherLink));
+      return fetchedWeather.data;
+    },
+    queryKey: [chosenCity.id],
+    onError: (e) => {
+      console.warn(e)
+    }
+  })
+  return (
+    <div>
+      <div>Temperature: {weather.data?.list[0].main.temp}</div>
+      <div>Feels like: {weather.data?.list[0].main.feels_like}</div>
+    </div>
+  )
+}
+
+export default WeatherInfoBar
