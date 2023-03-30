c6312881f9dceffa6303d2982e92e7d7996b8450
Yury Borets <udborets@gmail.com>
2023-03-30 17:05:38 +0300

    fix weather info content styles

--- components/WeatherInfoBar/WeatherInfoBar.tsx
+++ components/WeatherInfoBar/WeatherInfoBar.tsx
@@ -25,7 +25,7 @@ const WeatherInfoBar = () => {
     }
   })
   return (
-    <div className="weatherInfoBar p-4 rounded-[20px] bg-slate-300 w-[400px] h-[300px] shadow-2xl">
+    <div className="weatherInfoBar p-4 rounded-[20px] bg-slate-300 w-[700px] h-[300px] shadow-2xl">
       <div className="weatherInfoBar__container w-full h-full flex flex-col gap-4 items-center justify-center relative">
         {weather.isLoading
           ? <span>Loading...</span>
@@ -35,32 +35,25 @@ const WeatherInfoBar = () => {
             >
               {weather.data?.city.name}
             </h4>
-            <div className='weatherInfoBar__content flex flex-col'>
-              <ul className="weatherInfoBar__daySelectList flex">
-                <DaySelectButton
-                  isSelected={dayShowing === 0}
-                  onClick={() => setDayShowing(0)}
-                >
-                  Today, {weather.data?.list[0].dt_txt.split(" ")[0] ?? ""}
-                </DaySelectButton>
-                <DaySelectButton
-                  isSelected={dayShowing === 6}
-                  onClick={() => setDayShowing(6)}
-                >
-                  Tomorrow, {weather.data?.list[6].dt_txt.split(" ")[0] ?? ""}
-                </DaySelectButton>
-                <DaySelectButton
-                  isSelected={dayShowing === 15}
-                  onClick={() => setDayShowing(15)}
-                >
-                  DAT, {weather.data?.list[15].dt_txt.split(" ")[0] ?? ""}
-                </DaySelectButton>
-              </ul>
-              <ul className='weatherInfoBar__weatherInfo flex flex-col gap-1 bg-slate-600 p-4 rounded-b-xl text-white'>
-                <li>Temperature: {weather.data?.list[dayShowing].main.temp}&#8451;</li>
-                <li>Feels like: {weather.data?.list[dayShowing].main.feels_like}&#8451;</li>
-                <li>{weather.data?.list[dayShowing].weather[0].description}</li>
-              </ul>
+            <div className='weatherInfoBar__content flex w-full h-full justify-center items-center'>
+              <div className="weatherInfo flex flex-col w-2/3">
+                <ul className="weatherInfo__daySelectList flex">
+                  <DaySelectButton isSelected={dayShowing === 0} onClick={() => setDayShowing(0)} >
+                    Today, {weather.data?.list[0].dt_txt.split(" ")[0] ?? ""}
+                  </DaySelectButton>
+                  <DaySelectButton isSelected={dayShowing === 6} onClick={() => setDayShowing(6)} >
+                    Tomorrow, {weather.data?.list[6].dt_txt.split(" ")[0] ?? ""}
+                  </DaySelectButton>
+                  <DaySelectButton isSelected={dayShowing === 15} onClick={() => setDayShowing(15)} >
+                    DAT, {weather.data?.list[15].dt_txt.split(" ")[0] ?? ""}
+                  </DaySelectButton>
+                </ul>
+                <ul className='weatherInfo__info flex flex-col gap-1 bg-slate-600 p-4 rounded-b-xl text-white'>
+                  <li>Temperature: {weather.data?.list[dayShowing].main.temp}&#8451;</li>
+                  <li>Feels like: {weather.data?.list[dayShowing].main.feels_like}&#8451;</li>
+                  <li>{weather.data?.list[dayShowing].weather[0].description}</li>
+                </ul>
+              </div>
             </div>
           </>}
       </div>
