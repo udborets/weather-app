66a4cebd3111a27cf5964f992ab4738ab699df17
Yury Borets <udborets@gmail.com>
2023-03-30 12:07:36 +0300

    change home page classname

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import WeatherInfoBar from "@/components/WeatherInfoBar/WeatherInfoBar";
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex flex-col">
+    <div className="homePage__content w-full h-full flex flex-col">
       <WeatherInfoBar />
     </div>
   )
