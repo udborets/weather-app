88f3223e1f446b35084567edc6ed1b9bd9ba3f4c
Yury Borets <udborets@gmail.com>
2023-03-30 18:57:03 +0300

    fix day selection button props type

--- components/DaySelectButton/DaySelectButton.tsx
+++ components/DaySelectButton/DaySelectButton.tsx
@@ -1,17 +1,17 @@
 type DaySelectButtonProps = {
-  children: string | string[];
+  children: string | string[] | JSX.Element | JSX.Element[];
   onClick: (attr?: any) => any;
   isSelected: boolean;
 }
 
-const DaySelectButton = ({ children: text, onClick, isSelected }: DaySelectButtonProps) => {
+const DaySelectButton = ({ children, onClick, isSelected }: DaySelectButtonProps) => {
   return (
     <li className="daySelectButton h-fit w-fit">
       <button
         onClick={onClick}
         className={`daySelectButton__button h-fit w-fit py-1 px-2 text-black ${isSelected ? "bg-slate-600 rounded-t-xl text-white" : ""}`}
       >
-        {text}
+        {children}
       </button>
     </li>
   )
