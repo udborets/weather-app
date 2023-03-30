8ab762fd679f0afc40dd1126628c49d176288b83
Yury Borets <udborets@gmail.com>
2023-03-30 19:22:47 +0300

    change structure of day select button

--- components/DaySelectButton/DaySelectButton.tsx
+++ components/DaySelectButton/DaySelectButton.tsx
@@ -1,17 +1,25 @@
 type DaySelectButtonProps = {
-  children: string | string[] | JSX.Element | JSX.Element[];
   onClick: (attr?: any) => any;
   isSelected: boolean;
+  text: string;
+  date: string;
 }
 
-const DaySelectButton = ({ children, onClick, isSelected }: DaySelectButtonProps) => {
+const DaySelectButton = ({ text, date, onClick, isSelected }: DaySelectButtonProps) => {
   return (
     <li className="daySelectButton h-fit w-fit">
       <button
         onClick={onClick}
         className={`daySelectButton__button h-fit w-fit py-1 px-6 text-black ${isSelected ? "bg-slate-600 rounded-t-xl text-white" : ""}`}
       >
-        {children}
+        <div className="daySelectButton__content flex flex-col">
+          <span className="daySelectButton__dayText font-bold">
+            {text},
+          </span>
+          <span className="daySelectButton__date">
+            {date}
+          </span>
+        </div>
       </button>
     </li>
   )
