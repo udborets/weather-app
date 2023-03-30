67b71b152c4ba98bfaa22696fecc8ed0960a8a43
Yury Borets <udborets@gmail.com>
2023-03-30 18:58:00 +0300

    change padding

--- components/DaySelectButton/DaySelectButton.tsx
+++ components/DaySelectButton/DaySelectButton.tsx
@@ -9,7 +9,7 @@ const DaySelectButton = ({ children, onClick, isSelected }: DaySelectButtonProps
     <li className="daySelectButton h-fit w-fit">
       <button
         onClick={onClick}
-        className={`daySelectButton__button h-fit w-fit py-1 px-2 text-black ${isSelected ? "bg-slate-600 rounded-t-xl text-white" : ""}`}
+        className={`daySelectButton__button h-fit w-fit py-1 px-6 text-black ${isSelected ? "bg-slate-600 rounded-t-xl text-white" : ""}`}
       >
         {children}
       </button>
