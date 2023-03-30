85ad0e5ebea01fc988918b110f47906106d643f9
Yury Borets <udborets@gmail.com>
2023-03-30 11:04:52 +0300

    add day select button component

---
+++ components/DaySelectButton/DaySelectButton.tsx
@@ -0,0 +1,20 @@
+type DaySelectButtonProps = {
+  children: string;
+  onClick: (attr?: any) => any;
+  isSelected: boolean;
+}
+
+const DaySelectButton = ({ children: text, onClick, isSelected }: DaySelectButtonProps) => {
+  return (
+    <li className="daySelectButton h-fit w-fit">
+      <button
+        onClick={onClick}
+        className={`daySelectButton__button h-fit w-fit py-1 px-2 text-black ${isSelected ? "bg-slate-600 rounded-t-xl text-white" : ""}`}
+      >
+        {text}
+      </button>
+    </li>
+  )
+}
+
+export default DaySelectButton
