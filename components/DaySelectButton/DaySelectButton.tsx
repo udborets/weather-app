020f3231c51fba3016967d4c855e1e4fe8b66dbb
Yury Borets <udborets@gmail.com>
2023-04-15 20:28:35 +0300

    fix day select button text and styles

--- components/DaySelectButton/DaySelectButton.tsx
+++ components/DaySelectButton/DaySelectButton.tsx
@@ -1,7 +1,7 @@
 type DaySelectButtonProps = {
   onClick: (attr?: any) => any;
   isSelected: boolean;
-  text: string;
+  text?: string;
   date: string;
 }
 
@@ -10,13 +10,15 @@ const DaySelectButton = ({ text, date, onClick, isSelected }: DaySelectButtonPro
     <li className={`daySelectButton h-full w-fit ${isSelected ? "bg-slate-600 rounded-t-xl text-white" : ""}`}>
       <button
         onClick={onClick}
-        className={`daySelectButton__button h-fit w-fit py-1 px-6`}
+        className={`daySelectButton__button h-fit w-fit py-1 px-2 md::px-3`}
       >
         <div className="daySelectButton__content flex flex-col">
-          <span className="daySelectButton__dayText font-bold">
-            {text},
-          </span>
-          <span className="daySelectButton__date whitespace-nowrap">
+          {text
+            ? <span className="daySelectButton__dayText font-bold">
+              {text}
+            </span>
+            : ""}
+          <span className="daySelectButton__date font-bol whitespace-nowrap">
             {date}
           </span>
         </div>
