34b2b68324ca3186ab7c9d3e84e7a3492f517c29
Yury Borets <udborets@gmail.com>
2023-04-16 20:55:54 +0300

    fix day select button row styles and content

--- components/DaySelectButtonRow/DaySelectButtonRow.tsx
+++ components/DaySelectButtonRow/DaySelectButtonRow.tsx
@@ -9,19 +9,24 @@ interface DaySelectButtonRowProps {
 
 const DaySelectButtonRow = ({ fiveDaysInfo, dayShowing, setDayShowing }: DaySelectButtonRowProps) => {
   return (
-    <ul className="weatherInfo__daySelectList flex max-w-full w-fit h-fit">
-      {[...fiveDaysInfo.keys()].map((key, index) => {
-        const mapDayWeather = fiveDaysInfo.get(key);
-        if (mapDayWeather)
-          return <DaySelectButton
-            key={key}
-            isSelected={dayShowing === index}
-            onClick={() => setDayShowing(index)}
-            date={mapDayWeather[0].dt_txt.split(' ')[0].slice(5) ?? ""}
-            text={""}
-          />
-      })}
-    </ul>
+    <div className='weatherInfo flex flex-row gap-5 items-center'>
+      <span className="weatherInfo__text font-bold text-[1.2rem]">
+        Date:
+      </span>
+      <ul className="weatherInfo__daySelectList flex max-w-full w-fit h-fit gap-4">
+        {[...fiveDaysInfo.keys()].map((key, index) => {
+          const mapDayWeather = fiveDaysInfo.get(key);
+          if (mapDayWeather)
+            return <DaySelectButton
+              key={key}
+              isSelected={dayShowing === index}
+              onClick={() => setDayShowing(index)}
+              date={mapDayWeather[0].dt_txt.split(' ')[0].slice(5) ?? ""}
+              text={""}
+            />
+        })}
+      </ul>
+    </div>
   )
 }
 
