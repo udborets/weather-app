e5a9ae12f26b0a86b7dc6d9ce1ad699bf8974f2c
Yury Borets <udborets@gmail.com>
2023-04-16 15:37:38 +0300

    change day weather info styles

--- components/DayWeatherInfo/DayWeatherInfo.tsx
+++ components/DayWeatherInfo/DayWeatherInfo.tsx
@@ -6,9 +6,17 @@ interface DayWeatherInfoProps {
 }
 const DayWeatherInfo = ({ weatherInfo, className }: DayWeatherInfoProps) => {
   return (
-    <div className={"flex flex-col " + className}>
+    <div className={"flex flex-col gap-8 " + className}>
       {weatherInfo.map((timeInfo) => {
-        return (<span key={timeInfo.dt_txt}>{timeInfo.clouds.all}</span>)
+        return (
+          <div key={timeInfo.dt} className="text-white flex flex-col">
+            <span className="font-bold">{timeInfo.dt_txt.split(' ')[1]}</span>
+            <span className="font-bold">Temperature: </span>{timeInfo.main.temp}&#8451;
+            <span className="font-bold">Min temperature:</span> {timeInfo.main.temp_min}&#8451;
+            <span className="font-bold">Max temperature:</span> {timeInfo.main.temp_max}&#8451;
+            <span className="font-bold">Feels like:</span> {timeInfo.main.feels_like}&#8451;
+          </div>
+        )
       })}
     </div>
   )
