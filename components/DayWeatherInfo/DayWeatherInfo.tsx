9ecb3e1a1904b214f36ac235bd725e87ad55f3a6
Yury Borets <udborets@gmail.com>
2023-04-16 20:55:21 +0300

    fix day weather info component content and styles

--- components/DayWeatherInfo/DayWeatherInfo.tsx
+++ components/DayWeatherInfo/DayWeatherInfo.tsx
@@ -6,19 +6,35 @@ interface DayWeatherInfoProps {
 }
 const DayWeatherInfo = ({ weatherInfo, className }: DayWeatherInfoProps) => {
   return (
-    <div className={"flex flex-col gap-8 " + className}>
+    <ul className={"dayWeatherInfo__list flex flex-col gap-8 " + className}>
       {weatherInfo.map((timeInfo) => {
         return (
-          <div key={timeInfo.dt} className=" flex flex-col">
-            <span className="font-bold">{timeInfo.dt_txt.split(' ')[1]}</span>
-            <span className="font-bold">Temperature: </span>{timeInfo.main.temp}&#8451;
-            <span className="font-bold">Min temperature:</span> {timeInfo.main.temp_min}&#8451;
-            <span className="font-bold">Max temperature:</span> {timeInfo.main.temp_max}&#8451;
-            <span className="font-bold">Feels like:</span> {timeInfo.main.feels_like}&#8451;
+          <div
+            key={timeInfo.dt}
+            className="dayWeatherInfo__item w-fit h-fit p-8 flex flex-col bg-white rounded-[15px] outline outline-1"
+          >
+            <span className="font-bold">
+              Time: {timeInfo.dt_txt.split(' ')[1]}
+            </span>
+            <span className="font-bold">
+              Temperature:
+            </span>
+            {timeInfo.main.temp}&#8451;
+            <span className="font-bold">
+              Min temperature:
+            </span>
+            {timeInfo.main.temp_min}&#8451;
+            <span className="font-bold">
+              Max temperature:
+            </span>
+            {timeInfo.main.temp_max}&#8451;
+            <span className="font-bold">
+              Feels like:</span>
+            {timeInfo.main.feels_like}&#8451;
           </div>
         )
       })}
-    </div>
+    </ul>
   )
 }
 
