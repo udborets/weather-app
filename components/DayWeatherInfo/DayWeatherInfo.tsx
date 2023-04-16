9d6638a418cb3ccc0390c059ab2f116ba6d5863f
Yury Borets <udborets@gmail.com>
2023-04-16 18:18:40 +0300

    fix day weather info component props and styles

--- components/DayWeatherInfo/DayWeatherInfo.tsx
+++ components/DayWeatherInfo/DayWeatherInfo.tsx
@@ -2,14 +2,14 @@ import { WeatherListElement } from "@/models/weather";
 
 interface DayWeatherInfoProps {
   weatherInfo: WeatherListElement[];
-  className: string;
+  className?: string;
 }
 const DayWeatherInfo = ({ weatherInfo, className }: DayWeatherInfoProps) => {
   return (
     <div className={"flex flex-col gap-8 " + className}>
       {weatherInfo.map((timeInfo) => {
         return (
-          <div key={timeInfo.dt} className="text-white flex flex-col">
+          <div key={timeInfo.dt} className=" flex flex-col">
             <span className="font-bold">{timeInfo.dt_txt.split(' ')[1]}</span>
             <span className="font-bold">Temperature: </span>{timeInfo.main.temp}&#8451;
             <span className="font-bold">Min temperature:</span> {timeInfo.main.temp_min}&#8451;
